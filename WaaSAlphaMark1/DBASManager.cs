499aaae0d91689e5f437ee4b5048573dc250e4be
samvgs91 <samuelvgs91@gmail.com>
2021-08-25 22:27:48 -0500

    Changes in the way launch the Deploy Models

---
+++ WaaSAlphaMark1/DBASManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AnalysisServices.AdomdClient;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace WaaSAlphaMark1
+{
+    class DBASManager
+    {
+
+        public static AdomdConnection connection;
+        public static String connectionString;
+        public static String serverName;
+        public static String token;
+        static DBASManager()
+        {
+            //await GetDataFromAzureAnalysisService();
+            //   await ExecuteXMLAOnAzureAnalysisService() ;
+            serverName = "asazure://aspaaseastus2.asazure.windows.net/aasasrvpocea201";
+
+        }
+        private static async Task ExecuteXMLAOnAzureAnalysisService(String xmlJson)
+        {
+            //Grab the token
+            //Get servername from Azure Analysis Service (Overview) resource 
+            //Format: asazure://<region>.asazure.windows.net/<servername>
+            //var serverName = "asazure://aspaaseastus2.asazure.windows.net/aasasrvpocea201";
+            var token = await GetAccessToken("https://westeurope.asazure.windows.net");
+            var connectionString = $"Provider=MSOLAP;Data Source={serverName};Initial Catalog=WaaS Demo Database;User ID=;Password={token};Persist Security Info=True;Impersonation Level=Impersonate";
+
+            try
+            {
+                //read data from AAS
+                using (AdomdConnection connection = new AdomdConnection(connectionString))
+                {
+                    connection.Open();
+                    AdomdCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = xmlJson;
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static async Task<string> GetAccessToken(string aasUrl)
+        {
+            //var tenantId = "aa0fda5b-fa1f-4585-a8e7-3b8dfc42312e";
+            var appId = "72342ea8-14ff-40ca-86b8-e16cc62f00fb";
+            var appSecret = "db__KZ~oBT5Go3kG4v4F5r6VOjC~xIFFco";
+            string authorityUrl = $"https://login.microsoftonline.com/aa0fda5b-fa1f-4585-a8e7-3b8dfc42312e";
+
+            var authContext = new AuthenticationContext(authorityUrl);
+
+            // Config for OAuth client credentials 
+            var clientCred = new ClientCredential(appId, appSecret);
+            AuthenticationResult authenticationResult = await authContext.AcquireTokenAsync(aasUrl, clientCred);
+
+            //get access token
+            return authenticationResult.AccessToken;
+        }
+
+    }
+}
---
+++ WaaSAlphaMark1/DBManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaaSAlphaMark1
+{
+    class DBManager
+    {
+
+        public static SqlConnection sqlCon;
+        static DBManager()
+        {
+            sqlCon = new SqlConnection("Data Source=dccsrveu2taller03.database.windows.net;Initial Catalog=dccadbeu2taller03;Persist Security Info=True;User ID=adminusr;Password=@psstaller01");
+        }
+
+        public String GetBlobId(String fileName, String userId)
+        {
+            DataTable dtBlogTable = new DataTable();
+
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("[WaaS].[USP_WAAS_GET_BLOBID_BY_FILENAME]", sqlCon);
+            cmd.Parameters.AddWithValue("@fileName", fileName); // passing Datatable 
+            cmd.Parameters.AddWithValue("@userId", userId); // passing Datatable 
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dtBlogTable);
+            sqlCon.Close();
+            String blobId = dtBlogTable.Rows[0].Field<Guid>("BlobPathId").ToString();
+
+            return blobId;
+        }
+
+
+        public String GetXMLJson(String TableId, String UserId)
+        {
+            DataTable dtBlogTable = new DataTable();
+
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("[WaaS].[USP_WAAS_GET_MODEL_SCRIPT]", sqlCon);
+            cmd.Parameters.AddWithValue("@TableId", TableId); // passing Datatable 
+            cmd.Parameters.AddWithValue("@UserId", UserId); // passing Datatable 
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dtBlogTable);
+            sqlCon.Close();
+            String blobId = dtBlogTable.Rows[0].Field<Guid>("ModelCreationUptadeScript").ToString();
+
+            return blobId;
+        }
+
+    }
+}
